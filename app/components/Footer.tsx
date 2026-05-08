991f33817717dea30fe76be4b6a910d7eb71aa4c
Rushi Panara <panararushi44@gmail.com>
2026-05-08 16:41:20 +0530

    Delete app directory

--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-10 border-t border-white/10 text-center text-gray-500">
-      © 2026 SIEJA — Transforming Ideas Into Reality
-    </footer>
-  );
-}
