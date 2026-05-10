380bdd25d5f40126ae436d696d9f626f30b2ba56
RushiPanara <panararushi44@gmail.com>
2026-05-10 14:08:38 +0530

    fixed version

--- app/sections/Innovation.tsx
+++ app/sections/Innovation.tsx
@@ -17,11 +17,11 @@ export default function Innovation() {
 
           <p className="text-gray-400 leading-8">
             Every interaction inside SIEJA is designed to feel premium,
-            cinematic, immersive, and futuristic.Innovation is the power
-             to transform creative ideas into meaningful reality through 
-             technology, imagination, and intelligent thinking. It drives
-              progress by creating smarter solutions, futuristic experiences,
-               and new possibilities for the world.
+            cinematic, immersive, and futuristic. Innovation is the power
+            to transform creative ideas into meaningful reality through
+            technology, imagination, and intelligent thinking. It drives
+            progress by creating smarter solutions, futuristic experiences,
+            and new possibilities for the world.
           </p>
         </div>
       </div>
